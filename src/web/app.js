aeadcfd284f59247be9eba61fbbb2aaf3f5cb9dd
Wander Hillen <wjw.hillen@gmail.com>
2017-09-16 23:28:27 +0200

    Some bare bones frontend stuff

---
+++ src/web/app.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+
+  // Get references to elements on the page.
+  var form = document.getElementById('message-form');
+  var messageField = document.getElementById('message');
+  var messagesList = document.getElementById('messages');
+  var socketStatus = document.getElementById('status');
+  var closeBtn = document.getElementById('close');
+
+  // The rest of the code in this tutorial will go here...
+  // Create a new WebSocket.
+  var socket = new WebSocket('ws://localhost:3000/eventstream');
+  
+  // Show a connected message when the WebSocket is opened.
+  socket.onopen = function(event) {
+    socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.url;
+    socketStatus.className = 'open';
+  };
+
+  // Handle messages sent by the server.
+  socket.onmessage = function(event) {
+    var message = event.data;
+    messagesList.innerHTML += '<li class="received"><span>Received:</span>' +
+                               message + '</li>';
+  };
+  
+  // Handle any errors that occur.
+  socket.onerror = function(error) {
+    console.log('WebSocket Error: ' + error);
+  };
+};
