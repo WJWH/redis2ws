0555344167114c2b521da0798831d8fc4db7adcf
Wander Hillen <wjw.hillen@gmail.com>
2017-09-16 22:24:19 +0200

    Some initial code

--- src/redis2ws.cr
+++ src/redis2ws.cr
@@ -1,5 +1,29 @@
 require "./redis2ws/*"
+require "kemal"
 
 module Redis2ws
-  # TODO Put your code here
+  SOCKETS = [] of HTTP::WebSocket
+  
+  get "/" do |ctx|
+    "BIEP"
+  end
+  
+  ws "/chat" do |socket|
+    # Add the client to SOCKETS list
+    SOCKETS << socket
+
+    # Broadcast each message to all clients
+    socket.on_message do |message|
+      SOCKETS.each { |socket| socket.send message}
+    end
+
+    # Remove clients from the list when it’s closed
+    socket.on_close do
+      SOCKETS.delete socket
+    end
+  end
+
+  Kemal.run
 end
+
+
