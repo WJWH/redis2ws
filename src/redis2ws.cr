7dfcdb0ee46baf85667b837d25ce4cc438c44981
Wander Hillen <wjw.hillen@gmail.com>
2017-09-16 23:27:55 +0200

    Initial websockets endpoints

--- src/redis2ws.cr
+++ src/redis2ws.cr
@@ -1,20 +1,27 @@
-require "./redis2ws/*"
+#require "./redis2ws/*"
 require "kemal"
+require "redis"
 
 module Redis2ws
   SOCKETS = [] of HTTP::WebSocket
-  
+  puts "now here"
   get "/" do |ctx|
-    "BIEP"
+    send_file ctx, "src/web/index.html"
   end
   
-  ws "/chat" do |socket|
+  get "/app.js" do |ctx|
+    send_file ctx, "src/web/app.js"
+  end
+  
+  ws "/eventstream/" do |socket|
     # Add the client to SOCKETS list
+    puts "Received new connection!"
     SOCKETS << socket
 
     # Broadcast each message to all clients
     socket.on_message do |message|
-      SOCKETS.each { |socket| socket.send message}
+      # we don't really care about what clients send to us
+      #SOCKETS.each { |socket| socket.send message}
     end
 
     # Remove clients from the list when it’s closed
@@ -22,7 +29,19 @@ module Redis2ws
       SOCKETS.delete socket
     end
   end
-
+  
+  #run redis subscriber in its own fiber
+  spawn do
+    redis = Redis.new
+    redis.subscribe("mychannel") do |on|
+      on.message do |channel, message|
+        puts message
+        SOCKETS.each { |socket| socket.send message }
+      end
+    end
+  end
+  
+  puts "past redis"
   Kemal.run
 end
 
